1197a5f83533b0b46d16cf246d1d60ab719bf616
2181076H <114969043+2181076H@users.noreply.github.com>
2022-12-13 11:29:04 +0900

    content_script.js

--- 拡張機能１/content_script.js
+++ 拡張機能１/content_script.js
@@ -9,7 +9,22 @@ function f(n){
   for(let i = 0; i < cs.length; i++){
     let c = cs[i];
     if(c.nodeType == Node.TEXT_NODE){
-      c.textContent = c.textContent.replace(/です。/g, "でござるよ。");
+      c.textContent = c.textContent.replace(/です/g, "でござるよ");
+    }
+    else{
+      f(c);
+    }
+  }
+}
+
+f(document.body); // document.body は HTML の文章全体を示す要素
+
+function f(n){
+  let cs = n.childNodes;
+  for(let i = 0; i < cs.length; i++){
+    let c = cs[i];
+    if(c.nodeType == Node.TEXT_NODE){
+      c.textContent = c.textContent.replace(/である/g, "でござる");
     }
     else{
       f(c);
