b316fa71baf271bbda7689dd7869bf1881fda9ea
2181076H <114969043+2181076H@users.noreply.github.com>
2022-12-13 11:48:20 +0900

    content_script2.js

---
+++ 拡張機能１/content_script2.js
@@ -0,0 +1,11 @@
+let teacher = document.querySelectorAll(".teachers");
+for(let i = 0; i < teacher.length; i++){
+  let e = teacher[i];
+  e.style.display = 'none';
+}
+
+let sum = document.querySelectorAll(".summary");
+for(let i = 0; i < sum.length; i++){
+  let e = sum[i];
+  e.style.display = 'none';
+}
